e25fad3386f60dab551faf1d0e12f22b9e772acc
denzelIgnatius <denzel.ignatius@gmail.com>
2024-03-21 22:21:00 -0500

    Adding token generation for GetAuth API

--- controllers/tokenController.go
+++ controllers/tokenController.go
@@ -3,6 +3,8 @@ package controllers
 import (
 	"context"
 	"log"
+	"os"
+	"time"
 
 	"github.com/denzelIgnatius/auth_server_with_go/database"
 
@@ -11,6 +13,7 @@ import (
 	"github.com/denzelIgnatius/auth_server_with_go/models"
 	"github.com/denzelIgnatius/auth_server_with_go/validation"
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v4"
 	"go.mongodb.org/mongo-driver/bson"
 )
 
@@ -33,12 +36,36 @@ func GetAuth(c *gin.Context) {
 
 	if err != nil || len(result.Username) == 0 {
 		const errorMsg string = "Error: User does not exists"
-		log.Println(errorMsg)
+		log.Println(err)
 		c.JSON(400, errorMsg)
 		return
 	}
 
 	var isValidPassword bool = encryption.CompareHashedPassword(result.Password, request.Password)
 
-	c.JSON(200, isValidPassword)
+	if !isValidPassword {
+		const errorMsg string = "Error: Invalid credentials"
+		log.Println(errorMsg)
+		c.JSON(400, errorMsg)
+		return
+	}
+	expiration := time.Now().Add(time.Hour * 24 * 30)
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"Subject":    result.Username,
+		"Expiration": expiration.Unix(),
+	})
+
+	tokenString, error := token.SignedString([]byte(os.Getenv("SECRET")))
+	log.Println(tokenString)
+	if error != nil {
+		const errorMsg string = "Error: Token created failed with error"
+		log.Println(error)
+		c.JSON(500, errorMsg)
+		return
+	}
+
+	c.JSON(200, gin.H{
+		"token":      tokenString,
+		"expiration": expiration,
+	})
 }
